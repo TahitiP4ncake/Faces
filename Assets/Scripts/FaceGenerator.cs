2a20d7f7b6508741f9f764bc41d1ee2f3d49e5e8
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2019-05-14 19:29:18 +0200

    faces 01 pas fini

---
+++ Assets/Scripts/FaceGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceGenerator : MonoBehaviour
+{
+    public Part[] parts;
+
+    public GameObject[] eyes;
+    public GameObject[] noses;
+    public GameObject[] mouths;
+
+    public float etrange;
+    
+    public List<GameObject> activeFace = new List<GameObject>();
+    
+    void Start()
+    {
+        GenerateFace();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ClearFace();
+            GenerateFace();
+        }
+    }
+
+    void GenerateFace()
+    {
+
+        foreach (var part in parts)
+        {
+            part.Generate();
+        }
+        
+    }
+
+    public void AddPart(GameObject part, Transform _trans, bool _flip = false)
+    {
+        GameObject _part = Instantiate(part, _trans);
+        _part.transform.localPosition = Vector3.zero;
+
+        if (_flip)
+        {
+            _part.transform.localScale = new Vector3(-1,1,1);
+        }
+        activeFace.Add(_part);
+    }
+
+    void ClearFace()
+    {
+        foreach (var _face in activeFace)
+        {
+            Destroy(_face);
+        }
+        
+        activeFace.Clear();
+    }
+}
---
+++ Assets/Scripts/Part.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum partType
+{
+    eye,
+    mouth,
+    nose
+}
+
+public class Part : MonoBehaviour
+{
+
+    public bool active;
+    public float maxAngle = 1;
+    public float speed = .1f;
+
+    private float xoff;
+    private float yoff;
+    private float zoff;
+    
+    public FaceGenerator face;
+
+    public partType type;
+
+    private void Start()
+    {
+        xoff = Random.Range(0f, 100f);
+        yoff = Random.Range(0f, 100f);
+        zoff = Random.Range(0f, 100f);
+
+        speed = Random.Range(speed / 4, speed);
+
+    }
+
+
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(Mathf.PerlinNoise(0,Time.time*speed + xoff)*maxAngle, 
+                                                 Mathf.PerlinNoise(0, Time.time*speed + yoff)*maxAngle, 
+                                                 Mathf.PerlinNoise(0, Time.time*speed + zoff)*maxAngle);
+    }
+
+    public void Generate()
+    {
+        switch (type)
+        {
+            case partType.eye:
+                
+                break;
+            case partType.mouth:
+                
+                break;
+            case partType.nose:
+                
+                break;
+        }
+    }
+}
